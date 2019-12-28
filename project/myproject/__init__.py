ce91c9c9dcfb8aa91daa8aabade5c51e3c7a9bd3
codeXz <abahormelad@gmail.com>
2019-12-28 02:34:38 +0200

    fixing bugs

--- project/myproject/__init__.py
+++ project/myproject/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import SQLAlchemy
-
+from myproject.users.date import date
 # ----- initialization of app
 app = Flask(__name__)
 
@@ -14,13 +14,8 @@ db = SQLAlchemy(app)
 Migrate(app, db)
 
 # -------- configuration for db
-<<<<<<< HEAD
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://codeXz:hpprobook450g3*@127.0.0.1/thoughts'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-=======
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://codeXz:hpprobook450g3*@127.0.0.1/social'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
->>>>>>> fb76cf676707eecdc4d497e125ad225af6850874
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 
@@ -46,15 +41,12 @@ mail = Mail(app)
 login = LoginManager()
 login.init_app(app)
 login.login_view = 'users.login'
-<<<<<<< HEAD
 
-from myproject.users.users import users
-=======
 login.refresh_view = "users.change"
 from myproject.messages.messages import messages
 from myproject.users.users import users
 
 
 app.register_blueprint(messages)
->>>>>>> fb76cf676707eecdc4d497e125ad225af6850874
 app.register_blueprint(users)
+app.jinja_env.filters['date'] = date
